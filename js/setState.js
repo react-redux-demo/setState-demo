079449594b14cad0c99722079dc63cbd6284945d
liyanyan19940914 <921258351@qq.com>
2017-08-14 22:18:51 +0800

    increase 2

--- js/setState.js
+++ js/setState.js
@@ -4,9 +4,17 @@ class Count extends React.Component{
         this.state = {count:0}
     }
     handle(){
-        this.setState({count:this.state.count + 1},function () {
-            console.log(this.state.count);  //立即获取设置后的值
-        })
+        this.setState((prevState, props) => ({
+            count: prevState.count + 1
+
+        }));
+        this.setState((prevState, props) => ({
+            count: prevState.count + 1
+        }));
+        
+        // this.setState({count:this.state.count + 1},function () {
+        //     console.log(this.state.count);  //立即获取设置后的值
+        // })
         //console.log(this.state.count); //输出的是上一次的值，在setState中，这是一个异步处理的函数，并不是同步的，
         // console在setState后立刻执行了，所以这时候状态还没有真正变更完，所以这里取到的状态仍旧是更新前的
     }
